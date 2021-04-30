4625fb011ea54dee3588ff1dee80fd55c81e6ed0
romerocande13 <romerocande13@gmail.com>
2021-04-29 23:55:19 -0300

    Modificacion microservicios

--- Microservicios/WordService/WordReader.py
+++ Microservicios/WordService/WordReader.py
@@ -1,18 +1,18 @@
 import re
 import random
 
-def getWord():
-    path = '.\WordService\DataBase\words.txt'
-    with open(path) as file:
-        allData = file.read()
-        allData_array = re.split("\n",allData)
-        word = allData_array[random.randint(0,len(allData_array))]
-        result = [
-            {
-                'word': word
-            }
-        ]
-    return result
+# def getWord():
+#     path = '.\WordService\DataBase\words.txt'
+#     with open(path) as file:
+#         allData = file.read()
+#         allData_array = re.split("\n",allData)
+#         word = allData_array[random.randint(0,len(allData_array))]
+#         result = [
+#             {
+#                 'word': word
+#             }
+#         ]
+#     return result
 
 def getMultiplesWords(amount):
     path = '.\WordService\DataBase\words.txt'
--- Microservicios/WordService/WordService.py
+++ Microservicios/WordService/WordService.py
@@ -5,12 +5,8 @@ app = Flask(__name__)
 
 @app.route('/hangman/api/v1.0/word/<int:amountOfWords>', methods=['GET'])
 def getRandomWord(amountOfWords):
-    if amountOfWords == 1 :
-        word = getWord()
-    else:
-        word = getMultiplesWords(amountOfWords)
+    word = getMultiplesWords(amountOfWords)
     return jsonify(word)
-
 
 
 @app.errorhandler(404)
@@ -18,8 +14,5 @@ def not_found(error):
     return jsonify({'error': 'Error'}), 404
 
 
-
-
 if __name__ == '__main__':
-    #app.run(host= '0.0.0.0', debug=True)
     app.run(port=5000, debug=True)
