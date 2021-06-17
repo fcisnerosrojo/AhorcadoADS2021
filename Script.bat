818c39189f1150a66937a30b642658b52d04f2a2
Cande Romero <romerocande13@gmail.com>
2021-06-17 20:36:48 -0300

    Adding scripts

---
+++ Script.bat
@@ -0,0 +1,20 @@
+set pwd=%cd%
+set proyect=C:\Users\Cande\PROYECTOS\AhorcadoADS2021
+set virtualEnv=Microservicios\venv\Scripts
+set python_=C:\Users\Cande\AppData\Local\Programs\Python\Python38\python.exe
+set singlePlayerService=Microservicios\SingleplayerMatchService\SingleplayerMatchService.py
+set levelsService=Microservicios\LevelsService\LevelsService.py
+set wordService=Microservicios\WordService\WordService.py
+
+
+start "WordService" /d %pwd% "%proyect%\%wordService%"
+start "MatchService" /d %pwd% "%proyect%\%singlePlayerService%"
+start "LevelsService" /d %pwd% "%proyect%\%levelsService%"
+
+@REM start "MatchService" /d %pwd% "cd %pwd%\%virtualEnv% & activate.bat & cd /d %proyect% & python %proyect%\%matchService% "
+@REM start "LabelsService" /d %pwd% "cd %pwd%\%virtualEnv% & activate.bat & cd /d %proyect% & python %proyect%\%levelsService% "
+
+
+@REM  cmd /k "cd /d %pwd%\%virtualEnv% & activate.bat & cd /d %proyect% & python %proyect%\%wordService% "
+@REM cmd /k "cd /d %pwd%\%virtualEnv% & activate.bat & cd /d %proyect% & python %proyect%\%matchService% "
+@REM cmd /k "cd /d %pwd%\%virtualEnv% & activate.bat & cd /d %proyect% & python %proyect%\%levelsService% "
---
+++ getRequeriments.bat
@@ -0,0 +1,2 @@
+set python_=C:\Users\Cande\AppData\Local\Programs\Python\Python38\python.exe
+%python_% -m pip freeze > requirements.txt
